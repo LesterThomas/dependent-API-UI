2ae86efbdc53e0d34376fadc56de8d2847e605a0
Lester Thomas <lester.thomas@vodafone.com>
2023-01-02 20:15:18 +0000

    Added documentation for ExposedAPI mofule

--- dependent-api/src/components/common/ExposedAPIs/index.js
+++ dependent-api/src/components/common/ExposedAPIs/index.js
@@ -1,3 +1,6 @@
+/** 
+ * 2 Classes to display a list of ExposedAPIs and a single ExposedAPI respectively
+*/
 
 import React, { Component } from 'react';
 import DataTable from 'react-data-table-component';
@@ -7,12 +10,18 @@ import { CompTableWide, CompTDLeft, CompTDRight, CompTH, CompTR } from '../Style
 import { Link } from 'react-router-dom';
 
 function ResourceDetails(props) {
+  /**
+   * This helper component displays a single resource. The exact behaviour depends on the resource type.
+   */
   var resource = props.resource
   return (
     <div><RAGStatus size="10px" status={getResourceRAG(props.title, resource)} /> &nbsp; {resource.metadata.name}</div>
   );}
 
 function ResourcesDetails(props) {
+  /**
+   * This helper component displays a list of resources. The exact behaviour depends on the resource type.
+   */
   var resources = props.resources
   return (
     <div>
@@ -22,15 +31,21 @@ function ResourcesDetails(props) {
     </div>      
   );}
   
-  function getAPIRAG(inAPIStatusImplementation){
-    var ragColor = "amber"
-    if ((inAPIStatusImplementation) && (inAPIStatusImplementation.ready)) {
-      ragColor = "green"
-    }
-    return ragColor
+function getAPIRAG(inAPIStatusImplementation){
+  /**
+   * This is a helper function that returns the RAG status of an API based on its implementation ready status. 
+   */
+  var ragColor = "amber"
+  if ((inAPIStatusImplementation) && (inAPIStatusImplementation.ready)) {
+    ragColor = "green"
   }
+  return ragColor
+}
 
 function getResourceRAG(inTitle, inResource) {
+  /**
+   * This is a helper function that returns the RAG status of a resource. The exact behaviour depends on the resource type.
+   */
   var ragColor = "grey"
   switch (inTitle) {
 
@@ -77,6 +92,9 @@ function getResourceRAG(inTitle, inResource) {
   return ragColor
 }
 export class ExposedAPIs extends Component {
+  /**
+   * This class retrieves a list of exposed APIs and displays their metadata in a table.
+   */
 
   constructor(props) {
     super(props);
@@ -115,6 +133,10 @@ export class ExposedAPIs extends Component {
   };
 
   getExposedAPIs() {
+  /**
+  * Retrieves a list of exposed APIs and stores the data in the component's state.
+  * The data includes the API's component name, name, cluster URL, gateway URL, and status (ready or not ready).
+  */
     fetch(config.k8sAPIBaseUrl + config.ODAAPI + config.ODAAPIVersion + 'namespaces/' + config.ComponentsNamespace + config.ExposedAPIsResource)
       .then((res) => res.json())
       .then((json) => {
@@ -141,10 +163,18 @@ export class ExposedAPIs extends Component {
     }
 
   componentDidMount() {
+    /**
+     * This function is called when the component is mounted.
+     * It calls the getExposedAPIs function to retrieve the list of exposed APIs.
+     */
     this.getExposedAPIs();
   }
 
   render() {
+    /**
+     * This function renders the component.
+     * It returns a table containing the metadata for the exposed APIs.
+     */
     return (
   
       <div className="Component-body">
@@ -163,6 +193,15 @@ export class ExposedAPIs extends Component {
 };
 
 export class ExposedAPI extends Component {
+  /**
+   * A class for displaying and managing data for an ExposedAPI.
+   *
+   * The class fetches data for an ExposedAPI, including its metadata, specification, and associated resources (such as services
+   * and pods). The class also contains functions for refreshing the data and determining the RAG (red-amber-green) status of the
+   * ExposedAPI.
+   *
+   * @prop {string} name - The name of the ExposedAPI to display.
+   */
 
   constructor(props) {
     super(props);
@@ -171,6 +210,22 @@ export class ExposedAPI extends Component {
   };
 
   getExposedAPI() {
+    /**
+     * Fetches data for the ExposedAPI and updates the component state with the new data.
+     *
+     * The function constructs a URL using the ExposedAPI's name and the `config` object, and uses the `fetch` function to make a GET request to the URL. It then updates the component state with the data returned from the request.
+     *
+     * @param {string} config.k8sAPIBaseUrl - The base URL for the Kubernetes API.
+     * @param {string} config.ODAAPI - The name of the ODA API group.
+     * @param {string} config.ODAAPIVersion - The version of the ODA API.
+     * @param {string} config.ComponentsNamespace - The namespace for the ExposedAPI.
+     * @param {string} config.ExposedAPIsResource - The resource for ExposedAPIs.
+     * @param {string} this.state.exposedAPI.metadata.name - The name of the ExposedAPI.
+     * @param {string} config.CoreAPI - The name of the Kubernetes Core API group.
+     * @param {string} config.ServicesResource - The resource for services.
+     * @param {string} this.state.exposedAPI.spec.implementation - The service for the implementation of the ExposedAPI.
+     * @param {string} config.EndpointsResource - The resource for endpoints (services create enspoints that detail the implementation for the service).
+     */
     const queryAPIURL = config.k8sAPIBaseUrl + config.ODAAPI + config.ODAAPIVersion + 'namespaces/' + config.ComponentsNamespace + config.ExposedAPIsResource + this.state.exposedAPI.metadata.name
     console.log(queryAPIURL)
     fetch(queryAPIURL)
@@ -180,11 +235,20 @@ export class ExposedAPI extends Component {
         this.setState({exposedAPI: json})
         this.getResource(config.CoreAPI, config.ServicesResource, json.spec.implementation);
         this.getResource(config.CoreAPI, config.EndpointsResource, json.spec.implementation);
-  
       });
     }
 
   getResource(inAPI, inResource, inResourceName) {
+    /**
+     * Retrieves a resource specified by inResource from the Kubernetes API server,
+     * and sets it in the component's state. If inResource is a Service,
+     * then it also retrieves the corresponding pods with a selector 
+     * based on the service's spec.selector.
+     * 
+     * @param {string} inAPI - The base URL of the API server to fetch from.
+     * @param {string} inResource - The resource type to fetch.
+     * @param {string} inResourceName - The name of the resource to fetch.
+     */
     const queryAPIURL = config.k8sAPIBaseUrl + inAPI + 'namespaces/' + config.ComponentsNamespace + inResource + inResourceName
     console.log(queryAPIURL)
     fetch(queryAPIURL)
@@ -202,6 +266,15 @@ export class ExposedAPI extends Component {
     }
 
   getResources(inAPI, inResource, inResourceSelector, inResourceName) {
+    /**
+     * Retrieves a list of resources specified by inResource from the Kubernetes API server,
+     * and sets it in the component's state.
+     * 
+     * @param {string} inAPI - The base URL of the API server to fetch from.
+     * @param {string} inResource - The resource type to fetch.
+     * @param {string} inResourceSelector - The selector to use to filter the resources.
+     * @param {string} inResourceName - The name of the resource to fetch.
+    */
     const queryAPIURL = config.k8sAPIBaseUrl + inAPI + 'namespaces/' + config.ComponentsNamespace + inResource + inResourceSelector
     console.log(queryAPIURL)
     fetch(queryAPIURL)
@@ -209,26 +282,39 @@ export class ExposedAPI extends Component {
       .then((json) => {
         var newState = {}
         newState[inResourceName] = json.items
-        this.setState(newState)
-        
+        this.setState(newState)       
       });
     }  
 
   refreshData() {
+    /**
+     * Refreshes the data for the ExposedAPI.
+     */
     this.getExposedAPI();
-    // refresh the data every 5 seconds i
+    // refresh the data every 5 seconds
     var interval = 5000
     this.timeoutFunction = setTimeout(() => { this.refreshData() }, interval);
   }
 
   componentDidMount() {
+    /**
+     * Called when the component is mounted and loads the data for the ExposedAPI.
+     */ 
     this.refreshData();
   }
 
   componentWillUnmount() {
+    /**
+     * Called when the component is unmounted.
+     * Clears the timeout function.
+     */
     clearTimeout(this.timeoutFunction);
   }
+
   getRAG(inDeploymentStatus){
+    /**
+     * Returns the RAG status for the deployment.
+    */
     var ragColor = "amber"
     if (inDeploymentStatus === 'Complete') {
       ragColor = "green"
@@ -237,6 +323,19 @@ export class ExposedAPI extends Component {
   }
 
   render() {
+    /**
+     * The render() function is a method in a React component class that returns a JSX element to be rendered to the DOM.
+     * This function is called whenever the component's state or props are updated. This function
+     * renders the component for displaying an Exposed API. If the API is not found, a 404 message is displayed. 
+     * If the Service and Endpoints state objects are present, the Endpoints data is added to the Service status.
+     * The component includes the following information:
+     * - Component Name
+     * - Cluster URL
+     * - Gateway URL
+     * - Service
+     * - Pods
+     * @return {jsx} the JSX for the component
+     */ 
     var exposedAPI = this.state.exposedAPI
     // if the exposedAPI is not found then return a 404 message
     if ('code' in exposedAPI) {
@@ -262,8 +361,7 @@ export class ExposedAPI extends Component {
       }
     }
 
-
-    return (
+    return (   
       <div className="Component-body">
 
         <CompTableWide>
